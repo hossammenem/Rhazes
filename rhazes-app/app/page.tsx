d0914a2ff3c87e362031df58a75881ac5ea73466
hossammenem <hossammenem0102840@gmail.com>
2023-02-03 19:45:07 +0200

    trying out prettier changes after applying configs

--- rhazes-app/app/page.tsx
+++ rhazes-app/app/page.tsx
@@ -5,9 +5,5 @@ import styles from "./page.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <h1 className="flex h-screen w-screen self-center bg-black text-3xl font-bold underline">
-      Rhazes
-    </h1>
-  );
+  return <h1 className="flex h-screen w-screen self-center bg-black text-3xl font-bold underline">Rhazes</h1>;
 }
