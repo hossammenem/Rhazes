ed3402b46f9ed4a6716308cfdc72b3352fe3e4c9
hossammenem <hossammenem0102840@gmail.com>
2023-03-19 14:26:57 +0200

    changing the destination in the rewrite rule

--- rhazes-app/next.config.js
+++ rhazes-app/next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
     return [
       {
         source: "/en",
-        destination: `/${defaultLocale}`,
+        destination: `/`,
         permanent: true,
       },
       {
