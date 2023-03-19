e630e3a3474b20d280bd7d1128e6d118245b96f9
hossammenem <hossammenem0102840@gmail.com>
2023-03-19 13:40:08 +0200

    changing url for the API in apolloClient.ts and trying to fix the locales' bug

--- rhazes-app/ApolloClient.ts
+++ rhazes-app/ApolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { relayStylePagination } from "@apollo/client/utilities";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: "https://rhazes.vercel.app/api/graphql",
   cache: new InMemoryCache(),
 });
 
--- rhazes-app/next.config.js
+++ rhazes-app/next.config.js
@@ -6,14 +6,7 @@ const nextConfig = {
   i18n: {
     locales: ["en", "ar"],
     defaultLocale: "ar",
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/:lang(ar|en)/:path*",
-        destination: "/:path*",
-      },
-    ];
+    localeDetection: true,
   },
 };
 
