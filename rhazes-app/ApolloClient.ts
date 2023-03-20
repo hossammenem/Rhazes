2ed1c33880a700598da978808e8a173570a1fdae
hossammenem <88946630+hossammenem@users.noreply.github.com>
2023-03-20 23:20:34 +0200

    adding slash to the API URL

--- rhazes-app/ApolloClient.ts
+++ rhazes-app/ApolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { relayStylePagination } from "@apollo/client/utilities";
 
 const client = new ApolloClient({
-  uri: "https://rhazes.vercel.app/api/graphql",
+  uri: "https://rhazes.vercel.app/api/graphql/",
   cache: new InMemoryCache(),
 });
 
