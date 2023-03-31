5d57b17a637d6992971a736c257e512e0605417d
hossammenem <hossammenem0102840@gmail.com>
2023-03-31 13:28:08 +0200

    adjuting the orign string in apolloClient.ts

--- rhazes-app/ApolloClient.ts
+++ rhazes-app/ApolloClient.ts
@@ -1,9 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { relayStylePagination } from "@apollo/client/utilities";
 
-const origin = "https://rhazes.vercel.app"
-  ? process.env.NODE_ENV === "development"
-  : "http://localhost:3000";
+const origin =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://rhazes.vercel.app";
 
 const client = new ApolloClient({
   uri: `${origin}/api/graphql/`,
