e788b2cf5228866538e75e0b4892e0f628d53da1
hossammenem <hossammenem0102840@gmail.com>
2023-03-31 13:23:04 +0200

    adjuting origin to work in dev and prod env

--- rhazes-app/ApolloClient.ts
+++ rhazes-app/ApolloClient.ts
@@ -1,8 +1,12 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { relayStylePagination } from "@apollo/client/utilities";
 
+const origin = "https://rhazes.vercel.app"
+  ? process.env.NODE_ENV === "development"
+  : "http://localhost:3000";
+
 const client = new ApolloClient({
-  uri: "https://rhazes.vercel.app/api/graphql",
+  uri: `${origin}/api/graphql/`,
   cache: new InMemoryCache(),
 });
 
