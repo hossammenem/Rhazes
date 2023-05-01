8737af7da1151c56a6db23d5ba9bab6ad44a65d7
hossammenem <hossammenem0102840@gmail.com>
2023-05-01 20:33:25 +0300

    adjuting the about us section

--- rhazes-app/app/components/AboutUs.tsx
+++ rhazes-app/app/components/AboutUs.tsx
@@ -5,15 +5,18 @@ export default function AboutUs() {
 
   return (
     <section className="w-screen" id="about-us-section">
-      <h1 className="title-underline mx-auto pt-6 w-72 text-center text-4xl font-bold text-white">
+      <h1 className="title-underline mx-auto w-72 pt-6 text-center text-4xl font-bold text-white">
         {lang.aboutUsTitle}
       </h1>
-      <p
-        className="mx-auto p-8 text-center text-xl leading-8 text-white 
-        lg:w-2/3"
-      >
-        {lang.aboutUsDescription}
+      <div className="flex flex-col gap-6 p-7 mx-auto text-center text-xl leading-8 text-white 
+        lg:w-2/3">
+      <p>
+        {lang.aboutUsDescription.starting}
       </p>
+      <p>
+        {lang.aboutUsDescription.PS}
+      </p>
+      </div>
     </section>
   );
 }
--- rhazes-app/app/languages/en.ts
+++ rhazes-app/app/languages/en.ts
@@ -15,9 +15,16 @@ export default {
 
   // about us
   aboutUsTitle: "Who Are We?",
-  aboutUsDescription:
-    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit nostrum excepturi placeat, neque non est asperiores consectetur maiores facilis laudantium ut eius temporibus voluptates aliquam eos similique dicta necessitatibus eaque.",
-
+  aboutUsDescription: {
+    starting: `We here at Rhazes are an Arabic organization that's centred around the E-sports industry, Founded in 2023, Our Goals are to inspire people in the E-sports field, Build a community, and encourage them to improve, as well as share and unlock their creativity. Our most important value to hold is to keep a respectful and kind community. We have teams in various games like: Valorant, League of legends, Wild rift, and FIFA`,
+    PS: `and we will hopefully include more in the future!!!
+    Feel free to join us, and be a part of this community.
+    Together we can create and reach higher limits than each of us can alone.
+    Please don't hesitate to join us and try.
+    If you are interested about us
+    Follow us in our social media. 
+    `
+  },
   // news
   newsTitle: "Latest News",
 
